dd78f14a99836639ad38087625be21bd4d56f2c6
Kavya Muppalla <kavyamuppalla@zipcoders-MBP-2.fios-router.home>
2019-05-23 21:14:11 -0400

    Sum of the Numbers

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-
+     //int sum = 0;
     public static void main(String[] args){
-
+        System.out.println("Enter a number: ");
+        Scanner sr = new Scanner(System.in);
+        int n = sr.nextInt();
+        int sum = 0;
+        for(int i = 1; i <= n; i++)
+        {
+           System.out.println(i);
+            sum = sum + i;
+            //System.out.println(+sum);
+        }
+     
+        System.out.println("Sum " +sum);
+        
     }
 }
